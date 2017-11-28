25e0a9aadf3501c1f7d4bb8d76da57185e85993b
SorenSaket <sutensaket@gmail.com>
2017-11-28 22:08:10 +0100

    Trigonometry Drawing

--- drops/calculators/trigonometry/trigonometry.js
+++ drops/calculators/trigonometry/trigonometry.js
@@ -1,11 +1,128 @@
-function setup() {
-canvas = document.getElementById("maincanvas");
+function Calculate()
+{
+
 }
 
-function draw() {
-    ellipse(50, 50, 80, 80);
+function ThrowError(errorText)
+{
+
 }
 
+
+function solveTriangle(a, b, c, A, B, C) {
+	var sides  = (a != null) + (b != null) + (c != null);  // Boolean to integer conversion
+	var angles = (A != null) + (B != null) + (C != null);  // Boolean to integer conversion
+	var area, status;
+	
+	//Checks if equation is unsolvable
+	if (sides + angles != 3)
+		throw "Give exactly 3 pieces of information";
+	else if (sides == 0)
+		throw "Give at least one side length";
+	else if (sides == 3) 
+	{
+		status = "Side side side (SSS) case";
+		if (c >= a + b || a >= b + c || b >= c + a)
+			throw status + " - No solution";
+		A = solveAngle(b, c, a);
+		B = solveAngle(c, a, b);
+		C = solveAngle(a, b, c);
+		// Heron's formula
+		var s = (a + b + c) / 2;
+		area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+		
+	} else if (angles == 2) {
+		status = "Angle side angle (ASA) case";
+		// Find missing angle
+		if (A == null) A = 180 - B - C;
+		if (B == null) B = 180 - C - A;
+		if (C == null) C = 180 - A - B;
+		if (0 >= A || 0 >= B || 0 >= C)
+			throw status + " - No solution";
+		var sinA = Math.sin(degToRad(A));
+		var sinB = Math.sin(degToRad(B));
+		var sinC = Math.sin(degToRad(C));
+		// Use law of sines to find sides
+		var ratio;  // side / sin(angle)
+		if (a != null) { ratio = a / sinA; area = a * ratio * sinB * sinC / 2; }
+		if (b != null) { ratio = b / sinB; area = b * ratio * sinC * sinA / 2; }
+		if (c != null) { ratio = c / sinC; area = c * ratio * sinA * sinB / 2; }
+		if (a == null) a = ratio * sinA;
+		if (b == null) b = ratio * sinB;
+		if (c == null) c = ratio * sinC;
+		
+	} else if (and(A != null, a == null) || and(B != null, b == null) || and(C != null, c == null)) {
+		status = "Side angle side (SAS) case";
+		if (and(A != null, A >= 180) || and(B != null, B >= 180) || and(C != null, C >= 180))
+			throw status + " - No solution";
+		if (a == null) a = solveSide(b, c, A);
+		if (b == null) b = solveSide(c, a, B);
+		if (c == null) c = solveSide(a, b, C);
+		if (A == null) A = solveAngle(b, c, a);
+		if (B == null) B = solveAngle(c, a, b);
+		if (C == null) C = solveAngle(a, b, c);
+		if (A != null) area = b * c * Math.sin(degToRad(A)) / 2;
+		if (B != null) area = c * a * Math.sin(degToRad(B)) / 2;
+		if (C != null) area = a * b * Math.sin(degToRad(C)) / 2;
+		
+	} else {
+		status = "Side side angle (SSA) case - ";
+		var knownSide, knownAngle, partialSide;
+		if (and(a != null, A != null)) { knownSide = a; knownAngle = A; }
+		if (and(b != null, B != null)) { knownSide = b; knownAngle = B; }
+		if (and(c != null, C != null)) { knownSide = c; knownAngle = C; }
+		if (and(a != null, A == null)) partialSide = a;
+		if (and(b != null, B == null)) partialSide = b;
+		if (and(c != null, C == null)) partialSide = c;
+		if (knownAngle >= 180)
+			throw status + "No solution";
+		var ratio = knownSide / Math.sin(degToRad(knownAngle));
+		var temp = partialSide / ratio;  // sin(partialAngle)
+		var partialAngle, unknownSide, unknownAngle;
+		if (temp > 1 || and(knownAngle >= 90, partialSide >= knownSide))
+			throw status + "No solution";
+		else if (temp == 1 || knownSide >= partialSide) {
+			status += "Unique solution";
+			partialAngle = radToDeg(Math.asin(temp));
+			unknownAngle = 180 - knownAngle - partialAngle;
+			unknownSide = ratio * Math.sin(degToRad(unknownAngle));  // Law of sines
+			area = knownSide * partialSide * Math.sin(degToRad(unknownAngle)) / 2;
+		} else {
+			status += "Two solutions";
+			var partialAngle0 = radToDeg(Math.asin(temp));
+			var partialAngle1 = 180 - partialAngle0;
+			var unknownAngle0 = 180 - knownAngle - partialAngle0;
+			var unknownAngle1 = 180 - knownAngle - partialAngle1;
+			var unknownSide0 = ratio * Math.sin(degToRad(unknownAngle0));  // Law of sines
+			var unknownSide1 = ratio * Math.sin(degToRad(unknownAngle1));  // Law of sines
+			partialAngle = [partialAngle0, partialAngle1];
+			unknownAngle = [unknownAngle0, unknownAngle1];
+			unknownSide = [unknownSide0, unknownSide1];
+			area = [knownSide * partialSide * Math.sin(degToRad(unknownAngle0)) / 2,
+					knownSide * partialSide * Math.sin(degToRad(unknownAngle1)) / 2];
+		}
+		if (and(a != null, A == null)) A = partialAngle;
+		if (and(b != null, B == null)) B = partialAngle;
+		if (and(c != null, C == null)) C = partialAngle;
+		if (and(a == null, A == null)) { a = unknownSide; A = unknownAngle; }
+		if (and(b == null, B == null)) { b = unknownSide; B = unknownAngle; }
+		if (and(c == null, C == null)) { c = unknownSide; C = unknownAngle; }
+	}
+	
+	var circumference = a+b+c;
+
+	return [a, b, c, A, B, C, area, circumference, status];
+}
+
+function NumberOfUndefined(arrayOfAngles)
+{
+	var number = 0;
+	for (let index = 0; index < arrayOfAngles.length; index++) {
+		if(arrayOfAngles[index] == null)
+			number++;
+	}
+	return number;
+}
 
 function butCalculate_onclick() {
 	if (CheckForValidInput(frmTriangle.sideA.value,frmTriangle.sideB.value,frmTriangle.sideC.value,frmTriangle.angleA.value,frmTriangle.angleB.value))
@@ -42,7 +159,7 @@ function rad2deg(valRad){
 	return (360/(2*Math.PI)*valRad)
 }
 
-function deg2rad(valDeg){
+function degToRad(valDeg){
 	return ((2*Math.PI)/360*valDeg)
 }
 
---
+++ drops/calculators/trigonometry/trigonometryDraw.js
@@ -0,0 +1,139 @@
+var canvasSize = 512;
+var canvas;
+var scale = 30;
+var offsetX = 0;
+var offsetY = 0;
+var lastOffsetX = 0;
+var lastOffsetY = 0;
+var lastMousePosX = 0;
+var lastMousePosY = 0;
+var clicked = false;
+function setup() {
+	var canvas = createCanvas(canvasSize,canvasSize);
+	scale = 30;
+	canvas.mouseWheel(changeSize);
+}
+
+function draw() 
+{
+	background(255);
+	drawGrid(scale);
+	DrawTriangle(3.54, 7, 6.55, 30.09, 82, 67.91, scale);
+}
+
+function mouseDragged()
+{
+	if(!clicked)
+	{
+		lastMousePosX = mouseX;
+		lastMousePosY = mouseY;
+		lastOffsetX = offsetX;
+		lastOffsetY = offsetY;
+
+		clicked = true;
+	}
+	offsetX = lastOffsetX + (mouseX - lastMousePosX);
+	offsetY = lastOffsetY + (mouseY - lastMousePosY);
+}
+
+function mouseClicked() 
+{
+	clicked = false;
+}
+
+
+function changeSize(event)
+{
+	if (event.deltaY > 0) {
+		scale = scale + 5;
+	  } else {
+		scale = scale - 5;
+	  }
+}
+
+function drawGrid(scale)
+{
+	stroke(0,0,0);
+	noFill();
+	strokeWeight(1);
+	ellipse(CenterPosX() + offsetX,CenterPosY() + offsetY,scale*2,scale*2);
+}
+
+function DrawTriangle(a,b,c,A,B,C,scale)
+{
+	var textOffset = 10;
+	var pointSize = 8;
+	textSize(16);
+
+	var aColor = color(220,53,69);
+	var Ax = CenterPosX() + offsetX;
+	var Ay = CenterPosY()+ offsetY;
+	
+	var bColor = color(0,123,255);
+	var Bx = CenterPosX() + scale*b + offsetX;
+	var By = CenterPosY() + offsetY;
+
+	var cColor = color(40,167,69);
+	var Cx = CenterPosX()+(c*Math.cos(deg2rad(A))*scale)+ offsetX;
+	var Cy = CenterPosY()-(c*Math.sin(deg2rad(A))*scale)+ offsetY;
+	
+	// ---- Draw Triangle Background ---- 
+
+	noStroke();
+	fill(color("rgba(233,236,239, 0.6)"));
+	triangle(Ax,Ay,Cx,Cy,Bx,By);
+
+	// ---- Draw sides ---- 
+
+	strokeWeight(2);
+	
+	stroke(cColor);
+	line(Ax, Ay, Bx, By);
+
+	stroke(aColor);
+	line(Bx, By, Cx, Cy);
+
+	stroke(bColor);
+	line(Cx, Cy,Ax, Ay);
+
+	
+	// ---- Draw Points ----
+	
+	noStroke();
+
+	//A
+
+	fill(aColor);
+	ellipse(Ax,Ay,pointSize,pointSize)
+	text("A = " + A + ascii(176), Ax-textOffset , Ay+20)
+	text("a = " + a, (Bx+Cx)/2 + textOffset, (By+Cy)/2);
+
+	//B
+	
+	fill(bColor);
+	ellipse(Bx,By,pointSize,pointSize)
+	text("B = " + B + ascii(176), Bx+textOffset , By)
+	text("b = " + b, (Ax+Cx)/2 - textOffset*3, (Ay+Cy)/2 - textOffset);
+
+	//C
+
+	fill(cColor);
+	ellipse(Cx,Cy,pointSize,pointSize);
+	text("C = " + C + ascii(176), Cx , Cy-textOffset)	
+	text("c = " + c, (Ax+Bx)/2, (Ay+By)/2 + textOffset*2);
+}
+
+function CenterPosY()
+{
+	return height/2;
+}
+function CenterPosX()
+{
+	return width/2;
+}
+
+function deg2rad(valDeg){
+	return ((2*Math.PI)/360*valDeg)
+}
+
+function ascii (a) { return String.fromCharCode(a); }
