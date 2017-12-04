21a5362c738e2fea4351305733358631567c3e11
SorenSaket <sutensaket@gmail.com>
2017-12-04 19:36:04 +0100

    Commented and cleaned all code in Trigonometry.js

--- drops/calculators/trigonometry/trigonometry.js
+++ drops/calculators/trigonometry/trigonometry.js
@@ -1,23 +1,20 @@
-//
-//
-//
-//
-//
-/*if (c >= a + b || a >= b + c || b >= c + a)
-	displayError("You fucked up");
-A = solveAngle(a, b, c, "A");
-B = solveAngle(a, b, c, "B");
-C = solveAngle(a, b, c, "C");*/
+// ---------------- Variables ----------------
 
+//The priority of calculation of each element
 var calculationOrder = ["a", "b", "c", "A", "B", "C"];
+//The preferred algorithm to use when calculating a side
 var sideAlgorithmPreference = ["sin", "cos"];
+//The preferred algorithm to use when calculating an angle
 var angleAlgorithmPreference = ["180", "sin", "cos"];
+//The current triangle to calculate
 var currentTriangle;
 
-function calculate()
-{
+// ---------------- Functions to call ---------------
+
+// The function that the calculate button calls
+function calculate(){
 	//Gets input and sets it to a variable
-	var tempTri = GetInput();
+	var tempTri = getInput();
 	
 	console.log(tempTri);
 	
@@ -32,75 +29,20 @@ function calculate()
 		//Sets p5 to draw the triangle
 		triangleToDraw = tempTri;
 		
-		SetInput(tempTri);
+		setInput(tempTri);
 	
 		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 	}
 }
-
-function clearInputOutput()
-{
-	document.getElementById("a").value = "";
-	document.getElementById("b").value = "";
-	document.getElementById("c").value = "";
-	document.getElementById("A").value = "";
-	document.getElementById("B").value = "";
-	document.getElementById("C").value = "";
-	document.getElementById("area").value = "";
-	document.getElementById("circumference").value = "";
-	
-	document.getElementById("output").innerHTML = "";
+// The function that you can call from SSE
+function tri(a,b,c,A,B,C){
+	//wip
 }
 
-function GetInput()
-{	
-	var triangle = {
-		a: parseFloat(document.getElementById("a").value),
-		b: parseFloat(document.getElementById("b").value),
-		c: parseFloat(document.getElementById("c").value),
-		A: parseFloat(document.getElementById("A").value),
-		B: parseFloat(document.getElementById("B").value),
-		C: parseFloat(document.getElementById("C").value)
-	};
-	if(triangle.a == "" || triangle.a == 0 || isNaN(triangle.a))
-		triangle.a = null;
-	if(triangle.b == "" || triangle.b == 0 || isNaN(triangle.b))
-		triangle.b = null;
-	if(triangle.c == "" || triangle.c == 0 || isNaN(triangle.c))
-		triangle.c = null;
-	if(triangle.A == "" || triangle.A == 0 || isNaN(triangle.A))
-		triangle.A = null;
-	if(triangle.B == "" || triangle.B == 0 || isNaN(triangle.B))
-		triangle.B = null;
-	if(triangle.C == "" || triangle.C == 0 || isNaN(triangle.C))
-		triangle.C = null;
-	return triangle;
-}
+// ---------------- Calculation Functions ---------------
 
-function SetInput(triangle)
-{
-	document.getElementById("a").value = triangle.a;
-	document.getElementById("b").value = triangle.b;
-	document.getElementById("c").value = triangle.c;
-	document.getElementById("A").value = triangle.A;
-	document.getElementById("B").value = triangle.B;
-	document.getElementById("C").value = triangle.C;
-	document.getElementById("area").value = triangle.area;
-	document.getElementById("circumference").value = triangle.circumference;
-}
-
-function displayError(error) {
-	console.log(error);
-}
-
-function addLine(text) 
-{
-	//console.log(text);
-	document.getElementById("output").innerHTML += text;
-}
-
-function caluclateTriangle(triangle)
-{
+// The current way of calculation a triangle (Not working)
+function caluclateTriangle(triangle){
 	var a = triangle.a;
 	var b = triangle.b;
 	var c = triangle.c;
@@ -115,17 +57,17 @@ function caluclateTriangle(triangle)
 		case 0:
 			//SSS
 
-			A = solveAngle(a,b,c, "A");
+			A = calculateAngleCos(a,b,c, "A");
 			
-			B = solveAngle(a,b,c, "B");
+			B = calculateAngleCos(a,b,c, "B");
 			
-			C = solveAngleWith180(A,B,null);
+			C = calculateAngle180(A,B,null);
 			
 			break;
 		case 1:
 			//Find the missing side
-			var unknownSide = GetUnknownSide(a,b,c);
-			var knownAngle = GetKnownAngle(A,B,C);
+			var unknownSide = getUnknownSide(a,b,c);
+			var knownAngle = getKnownAngle(A,B,C);
 
 			//Check if the missing side's opposing angle is null
 			if(isOppositeAngleNull(A,B,C,unknownSide))
@@ -150,7 +92,7 @@ function caluclateTriangle(triangle)
 					);
 					
 					//Calulate The last angle with 180
-					C = solveAngleWith180(A,B,C);
+					C = calculateAngle180(A,B,C);
 				}
 				if(knownAngle == "B")
 				{
@@ -170,7 +112,7 @@ function caluclateTriangle(triangle)
 					);
 					
 					//Calulate The last angle with 180
-					C = solveAngleWith180(A,B,C);
+					C = calculateAngle180(A,B,C);
 				}
 				if(knownAngle == "C")
 				{
@@ -190,7 +132,7 @@ function caluclateTriangle(triangle)
 					);
 
 					//Calulate The last angle with 180
-					B = solveAngleWith180(A,B,C);
+					B = calculateAngle180(A,B,C);
 				}
 
 
@@ -206,11 +148,11 @@ function caluclateTriangle(triangle)
 			break;
 		case 2:
 			if (A == null)
-				A = solveAngleWith180(A,B,C);
+				A = calculateAngle180(A,B,C);
 			if (B == null) 
-				B = solveAngleWith180(A,B,C);
+				B = calculateAngle180(A,B,C);
 			if (C == null)
-				C = solveAngleWith180(A,B,C);
+				C = calculateAngle180(A,B,C);
 
 			if (A <= 0 || B <= 0 || C <= 0)
 				displayError("No solution");
@@ -283,19 +225,16 @@ function caluclateTriangle(triangle)
 		A: A,
 		B: B,
 		C: C,
-		area: calculateAreaWithSides(a,b,c),
+		area: calculateAreaHeron(a,b,c),
 		circumference: calculateCircumference(a,b,c)
 	}
 
 	return tempTriangle;
 }
-
-function caluclateTriangle1(triangle)
-{
-
-
+// The NEW way of calculation a triangle (Not working either)
+function caluclateTriangle1(triangle){	
 	for (let x = 0; x < calculationOrder.length; x++) {
-		calculateElement(calculationOrder[x]);
+		triangle = calculateElement(triangle, calculationOrder[x]);
 	}
 	
 	var tempTriangle =
@@ -306,27 +245,33 @@ function caluclateTriangle1(triangle)
 		A: A,
 		B: B,
 		C: C,
-		area: calculateAreaWithSides(a,b,c),
+		area: calculateAreaHeron(a,b,c),
 		circumference: calculateCircumference(a,b,c)
 	}
 
 	return tempTriangle;
 }
+//WIP
+function calculateElement(triangle, elementToCalculate){
 
-function calculateElement(triangle, elementToCalculate)
-{
-	var a = triangle.a;
-	var b = triangle.b;
-	var c = triangle.c;
-	var A = triangle.A;
-	var B = triangle.B;
-	var C = triangle.C;
-
-	if(elementToCalculate == "a" && a == null)
+	if(elementToCalculate == "a" && triangle.a == null)
 	{
 		//Sin
+		var al = determineUsedAlgorithm(triangle, elementToCalculate)
 		
-
+		for (let x = 0; x < al.neededVars.length; x++) {
+			triangle = calculateElement(triangle, al.neededVars[x]);
+		}
+		
+		if(al.algorithm == "sin")
+		{
+			//Use b/sin(B) or c/sin(C)
+			//solveSideSin(a,b,c,A,B,C, "a");
+		}
+		else if(al.algorithm == "cos")
+		{
+			a = calculateSideCos(/*wip*/);
+		}
 	}
 	else if(elementToCalculate == "b" && b == null)
 	{
@@ -348,118 +293,103 @@ function calculateElement(triangle, elementToCalculate)
 	{
 		
 	}
-}
 
-function GetUnknownSide(a,b,c)
-{
-	if(!a)
-		return "a";
-	else if(!b)
-		return "b";
-	else
-		return "c"
+	return triangle;
 }
-
-function GetUnknownAngle(A,B,C)
-{
-	if(!A)
-		return "A";
-	else if(!B)
-		return "B";
-	else
-		return "C"
-}
-
-function GetKnownAngle(A,B,C)
-{
-	if(A)
-		return "A";
-	else if(B)
-		return "B";
-	else
-		return "C"
-}
-
-function isOppositeAngleNull(A, B, C, side)
-{
-	switch (side) {
-		case "a":
-			if(A == null)
-				return true;
-			break;
-		case "b":
-			if(B == null)
-				return true;
-			break;
-		case "c":
-			if(C == null)
-				return true;
-			break;
-		default:
-			console.log("You must input a side: a, b or c");
-			return false;
-			break;
-	}
-	return false;
-}
-
-function isOppositeSideNull(a, b, c, angle)
-{
-	switch (angle) {
-		case "A":
-			if(a == null)
-				return true;
-			break;
-		case "B":
-			if(b == null)
-				return true;
-			break;
-		case "C":
-			if(c == null)
-				return true;
-			break;
-		default:
-			console.log("You must input an angle: A, B or C");
-			return false;
-			break;
-	}
-	return false;
-}
-
-function isUnsolvable(triangle)
-{
-	var sides  = (triangle.a != null) + (triangle.b != null) + (triangle.c != null);  // Boolean to integer conversion
-	var angles = (triangle.A != null) + (triangle.B != null) + (triangle.C != null);  // Boolean to integer conversion
-	//Checks if equation is unsolvable
-	if (sides + angles != 3)
+// Determine the algorithm to use based on preferences and missing elements
+function determineUsedAlgorithm(triangle, elementToCalculate){
+	if(sideAlgorithmPreference[0] == "sin")
 	{
-		displayError("Give at least 3 pieces of information");
-		return true;
-	}
-	else if (sides == 0)
-	{
-		displayError("Give at least one side length");
-		return true;
+		if(triangle)
+
+
+
 	}
 	else
 	{
-		return false;
-	}
-}
 
-// Returns side c using law of cosines
-function solveSide(a, b, C) 
-{
+	}
+
+
+
+	var temp =
+	{
+
+
+	}
+
+	return temp;
+}
+// Returns side c using law of cosines WIP
+function calculateSide(a, b, C) {
 	C = degToRad(C);
 	if (C > 0.001)
 		return Math.sqrt(a * a + b * b - 2 * a * b * Math.cos(C));
 	else // Explained in https://www.nayuki.io/page/numerically-stable-law-of-cosines
 		return Math.sqrt((a - b) * (a - b) + a * b * C * C * (1 - C * C / 12));
 }
+// WIP
+function calculateSideCos(a, b, C) {
+	C = degToRad(C);
+	if (C > 0.001)
+		return Math.sqrt(a * a + b * b - 2 * a * b * Math.cos(C));
+	else // Explained in https://www.nayuki.io/page/numerically-stable-law-of-cosines
+		return Math.sqrt((a - b) * (a - b) + a * b * C * C * (1 - C * C / 12));
+}
+// Returns side with sinus rule WIP
+function calculateSideSin(a,b,c,A,B,C, sideToSolve){
+	var sinA = Math.sin(degToRad(A));
+	var sinB = Math.sin(degToRad(B));
+	var sinC = Math.sin(degToRad(C));
+	
+	// Use law of sines to find sides
+	var ratio; // side / sin(angle)
+	
+	if (a != null) 
+	{
+		ratio = a / sinA;
+		area = a * ratio * sinB * sinC / 2;
+		b = ratio * sinB;
+		c = ratio * sinC;
+		addLine("//Calculate b");
+		addLine("$$b = {a \\over {\\sin (A)}}*\\sin (B)$$");
+		addLine("$$"+ b+ " = {"+ a+" \\over {\\sin (" + A + ")}}*\\sin ("+ B + ")$$");
+		
+		addLine("//Calculate c");
+		addLine("$$c = {a \\over {\\sin (A)}}*\\sin (C)$$");
+		addLine("$$"+ c+ " = {"+ a+" \\over {\\sin (" + A + ")}}*\\sin ("+ C + ")$$");
+	}
+	else if (b != null) 
+	{
+		ratio = b / sinB;
+		area = b * ratio * sinC * sinA / 2;
+		a = ratio * sinA;
+		c = ratio * sinC;
+		addLine("//Calculate a");
+		addLine("$$a = {b \\over {\\sin (B)}}*\\sin (A)$$");
+		addLine("$$" + a + " = {" + b +" \\over {\\sin (" + B +")}}*\\sin (" + A +")$$");
 
+		addLine("//Calculate c");
+		addLine("$$c = {b \\over {\\sin (B)}}*\\sin (C)$$");
+		addLine("$$" + c + " = {" + b +" \\over {\\sin (" + B +")}}*\\sin (" + C +")$$");
+	}
+	else if (c != null) 
+	{
+		ratio = c / sinC;
+		area = c * ratio * sinA * sinB / 2;
+		a = ratio * sinA;
+		b = ratio * sinB;
+		addLine("//Calculate a");
+		addLine("$$a = {c \\over {\\sin (C)}}*\\sin (A)$$");
+		addLine("$$" + a + " = {" + c + " \\over {\\sin (" + C + ")}}*\\sin (" + A + ")$$");
+		
+		addLine("//Calculate b");
+		addLine("$$b = {c \\over {\\sin (C)}}*\\sin (B)$$");
+		addLine("$$" + b + " = {" + c + " \\over {\\sin (" + C + ")}}*\\sin (" + B + ")$$");
+	}
+}
 // Returns angle C using law of cosines
-function solveAngle(a, b, c, angleToCalculate) 
-{
+function calculateAngleCos(a, b, c, angleToCalculate) {
 	var temp;
 	addLine("// --- Calculate " + angleToCalculate +", using law of cosines ---");
 	switch (angleToCalculate) {
@@ -483,9 +413,8 @@ function solveAngle(a, b, c, angleToCalculate)
 	}
 	return radToDeg(Math.acos(temp));
 }
-
-function solveAngleWith180(A,B,C)
-{
+//Returns the missing angle with the 180 rule
+function calculateAngle180(A,B,C){
 	if (A == null)
 	{
 		A = 180 - B - C;
@@ -531,9 +460,8 @@ function solveAngleWith180(A,B,C)
 		return C;
 	}
 }
-
-function calculateAreaWithSides(a,b,c)
-{
+//Returns the area of a triangle with Heron's formula
+function calculateAreaHeron(a,b,c){
 	var s = (a + b + c)/2;
 	var t = s * (s - a) * (s - b) * (s - c)
 	var area = Math.sqrt(t);
@@ -557,9 +485,8 @@ function calculateAreaWithSides(a,b,c)
 
 	return area;
 }
-
-function calculateCircumference(a,b,c)
-{
+//Returns the cirumfrence of a triangle
+function calculateCircumference(a,b,c){
 	circumference = a + b + c;
 	addLine("//Calculate circumference");
 	addLine
@@ -572,10 +499,228 @@ function calculateCircumference(a,b,c)
 	return circumference;
 }
 
+// ---------------- Output/Input ----------------
+
+// Clears all inputs and outputs (Resets the calculator)
+function clearInputOutput(){
+	//Clear input
+	document.getElementById("a").value = "";
+	document.getElementById("b").value = "";
+	document.getElementById("c").value = "";
+	document.getElementById("A").value = "";
+	document.getElementById("B").value = "";
+	document.getElementById("C").value = "";
+	document.getElementById("area").value = "";
+	document.getElementById("circumference").value = "";
+	
+	//Clear ouputs
+	document.getElementById("output").innerHTML = "";
+	
+	triangleToDraw = null;
+}
+// Returns a triangle from input
+function getInput(){
+	var triangle = {
+		a: parseFloat(document.getElementById("a").value),
+		b: parseFloat(document.getElementById("b").value),
+		c: parseFloat(document.getElementById("c").value),
+		A: parseFloat(document.getElementById("A").value),
+		B: parseFloat(document.getElementById("B").value),
+		C: parseFloat(document.getElementById("C").value)
+	};
+	if(triangle.a == "" || triangle.a == 0 || isNaN(triangle.a))
+		triangle.a = null;
+	if(triangle.b == "" || triangle.b == 0 || isNaN(triangle.b))
+		triangle.b = null;
+	if(triangle.c == "" || triangle.c == 0 || isNaN(triangle.c))
+		triangle.c = null;
+	if(triangle.A == "" || triangle.A == 0 || isNaN(triangle.A))
+		triangle.A = null;
+	if(triangle.B == "" || triangle.B == 0 || isNaN(triangle.B))
+		triangle.B = null;
+	if(triangle.C == "" || triangle.C == 0 || isNaN(triangle.C))
+		triangle.C = null;
+	return triangle;
+}
+// Sets input to triangle values
+function setInput(triangle){
+	document.getElementById("a").value = triangle.a;
+	document.getElementById("b").value = triangle.b;
+	document.getElementById("c").value = triangle.c;
+	document.getElementById("A").value = triangle.A;
+	document.getElementById("B").value = triangle.B;
+	document.getElementById("C").value = triangle.C;
+	document.getElementById("area").value = triangle.area;
+	document.getElementById("circumference").value = triangle.circumference;
+}
+// Displays an error (just console.logs for now)
+function displayError(error) {
+	console.log(error);
+}
+// Adds a line to the output
+function addLine(text) {
+	document.getElementById("output").innerHTML += text;
+}
+
+// ---------------- Helper Functions (Not all of these are used) ----------------
+
+//Returns a bool depending if triangle is unsolvable
+function isUnsolvable(triangle){
+	var sides  = (triangle.a != null) + (triangle.b != null) + (triangle.c != null);  // Boolean to integer conversion
+	var angles = (triangle.A != null) + (triangle.B != null) + (triangle.C != null);  // Boolean to integer conversion
+	if (sides + angles < 3)
+	{
+		displayError("Give at least 3 pieces of information");
+		return true;
+	}
+	else if (sides == 0)
+	{
+		displayError("Give at least one side length");
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//Returns the opposite side of side as a string
+function getOppositSide(angle){
+	if(side == "A")
+		return "a";
+	else if(side == "B")
+		return "b";
+	else if(side == "C")
+		return "c";
+	else
+		return null;
+}
+//Returns the opposite angle of side as a string
+function getOppositeAngle(side){
+	if(side == "a")
+		return "A";
+	else if(side == "b")
+		return "B";
+	else if(side == "c")
+		return "C";
+	else
+		return null;
+}
+//Returns the unknown side from 3 sides as string
+function getUnknownSide(a,b,c){
+	if(!a)
+		return "a";
+	else if(!b)
+		return "b";
+	else
+		return "c"
+}
+//Returns the unknown angle from 3 angles as string
+function getUnknownAngle(A,B,C){
+	if(!A)
+		return "A";
+	else if(!B)
+		return "B";
+	else if(!C)
+		return "C"
+	else
+	{
+		console.log("There are no unknown angles");
+		return null;
+	}
+}
+//Returns the known side from 3 side as string
+function getKnownSide(A,B,C){
+	if(A)
+		return "A";
+	else if(B)
+		return "B";
+	else if(C)
+		return "C"
+	else
+	{
+		console.log("There are no known sides");
+		return null;
+	}
+}
+//Returns the known angle from 3 angles as string
+function getKnownAngle(A,B,C){
+	if(A)
+		return "A";
+	else if(B)
+		return "B";
+	else if(C)
+		return "C"
+	else
+	{
+		console.log("There are no known angles");
+		return null;
+	}
+}
+//Returns a bool if the opposite side of an angle is null
+function isOppositeSideNull(a, b, c, angle){
+	switch (angle) {
+		case "A":
+			if(a == null)
+				return true;
+			break;
+		case "B":
+			if(b == null)
+				return true;
+			break;
+		case "C":
+			if(c == null)
+				return true;
+			break;
+		default:
+			console.log("You must input an angle: A, B or C");
+			return false;
+			break;
+	}
+	return false;
+}
+//Returns a bool if the opposite angle of a side is null
+function isOppositeAngleNull(A, B, C, side){
+	switch (side) {
+		case "a":
+			if(A == null)
+				return true;
+			break;
+		case "b":
+			if(B == null)
+				return true;
+			break;
+		case "c":
+			if(C == null)
+				return true;
+			break;
+		default:
+			console.log("You must input a side: a, b or c");
+			return false;
+			break;
+	}
+	return false;
+}
+//Converts radians to degrees
 function radToDeg(valRad) {
 	return (360 / (2 * Math.PI) * valRad)
 }
-
+//Converts degrees to radians
 function degToRad(valDeg) {
 	return ((2 * Math.PI) / 360 * valDeg)
+}
+//WIP
+function sin(inputfloat){
+	//wip
+}
+//WIP
+function arcsin(inputfloat){
+	//wip
+}
+//WIP
+function cos(inputfloat){
+	//wip
+}
+//WIP 
+function arccos(inputfloat){
+	//wip
 }
