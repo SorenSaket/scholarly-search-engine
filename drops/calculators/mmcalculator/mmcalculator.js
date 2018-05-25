d6b53bfc619f99ec9653d4b3c0dfce1723bc81a3
Soren Saket <sutensaket@gmail.com>
2018-05-25 14:14:34 +0200

    MMCaluclator 0.1.0

---
+++ drops/calculators/mmcalculator/mmcalculator.js
@@ -0,0 +1,131 @@
+var elements = [
+    H = {
+        'M': 1.0079
+    },
+    He = 
+    {
+        'M': 4.0026
+    },
+    Li = 
+    {
+        'M': 6.941
+    },
+    Be = 
+    {
+        'M': 9.0122
+    },
+    B = 
+    {
+        'M': 10.811
+    },
+    C = 
+    {
+        'M': 12.0107
+    },
+    N = 
+    {
+        'M': 14.0067
+    },
+    O = 
+    {
+        'M': 15.9994
+    },
+    F = 
+    {
+        'M': 18.9984
+    },
+    Ne = 
+    {
+        'M': 20.1797
+    },
+    Na = 
+    {
+        'M': 22.9897
+    },
+    Mg = 
+    {
+        'M': 24.305
+    },
+    Al = 
+    {
+        'M': 26.9815
+    },
+    Si = 
+    {
+        'M': 28.0855
+    },
+    P = 
+    {
+        'M': 30.9738
+    },
+    S = 
+    {
+        'M': 32.065
+    },
+    Cl = 
+    {
+        'M': 35.453
+    },
+    K = 
+    {
+        'M': 39.0983
+    },
+    Ar = 
+    {
+        'M': 39.948
+    },
+    Ca = 
+    {
+        'M': 40.078
+    },
+    Sc = 
+    {
+        'M': 44.9559
+    },
+    Ti = 
+    {
+        'M': 47.867
+    },
+    V = 
+    {
+        'M': 50.9415
+    },
+    Cr = 
+    {
+        'M': 51.9961
+    },
+    Mn = 
+    {
+        'M': 54.938
+    }
+
+];
+
+var currentElements = [];
+
+
+function AddElement()
+{
+    var element = {
+        'm': 0, 
+        'M': 0,
+        'n': 0
+    };
+    document.getElementById("element").innerHTML += '<th scope="col">#</th>'
+
+}
+
+function GetMolarMass()
+{
+
+}
+
+
+function Calculate()
+{
+    for (let i = 0; i < currentElements.length; i++) {
+        element
+        
+    }
+
+}
